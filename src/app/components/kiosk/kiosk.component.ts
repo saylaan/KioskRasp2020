a9d6235dc6ece8a659b00f4c2457a86fc8ed2314
VolkiReplay <volkireplay@localhost.localdomain>
2020-03-03 16:59:01 +0100

    push change directory

--- src/app/components/kiosk/kiosk.component.ts
+++ src/app/components/kiosk/kiosk.component.ts
@@ -14,6 +14,7 @@ import {
 @Component({
   selector: 'app-api',
   templateUrl: './kiosk.component.html',
+  styleUrls: ['./kiosk.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
